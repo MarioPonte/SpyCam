022f1b5aed36012d2e562fdb76e870a0922aa9e7
Mário Ponte <ponteolavo30@gmail.com>
2022-07-13 20:11:26 +0100

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,18 @@ navigator.mediaDevices.getUserMedia({video:true})
     console.log(error);
 });
 
+var link;
+
 document.querySelector('button').addEventListener('click', () => {
     var canvas = document.querySelector('canvas');
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     var context = canvas.getContext('2d');
     context.drawImage(video, 0, 0);
-    var link = document.createElement('a');
+    if(link!=undefined){
+        link.remove();
+    }
+    link = document.createElement('a');
     link.download = "foto.png";
     link.href = canvas.toDataURL("image/jpeg", 0.1);
     link.textContent = "Clique para baixar a foto";
@@ -24,14 +29,12 @@ document.querySelector('button').addEventListener('click', () => {
     var tempParams = {
         to_name:"Mário",
         message:"foto",
-        content: "google.com",
+        content: link.href,
     };
 
     console.log(link.href);
 
-    /*
     emailjs.send('service_2ua9jwp','template_xc2i0cq',tempParams).then(function(res){
         console.log("sucess", res.status);
     });
-    */
 });
