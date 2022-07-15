849f1c940d40360de18167390e41ec9263ad8916
Mário Ponte <ponteolavo30@gmail.com>
2022-07-15 20:07:06 +0100

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+(function(){
+    emailjs.init("m9JcXhx2n9L6gkRHC");
+})();
+
 var video = document.querySelector('video');
 
 navigator.mediaDevices.getUserMedia({video:true})
@@ -11,7 +15,14 @@ navigator.mediaDevices.getUserMedia({video:true})
 
 var link;
 
-document.querySelector('button').addEventListener('click', () => {
+var blur = document.getElementById("blur");
+var brightness = document.getElementById("brightness");
+var contrast = document.getElementById("contrast");
+var invert = document.getElementById("invert");
+var saturate = document.getElementById("saturate");
+var sepia = document.getElementById("sepia");
+
+document.getElementById('btnPhotoTake').addEventListener('click', () => {
     var canvas = document.querySelector('canvas');
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
@@ -37,4 +48,28 @@ document.querySelector('button').addEventListener('click', () => {
     emailjs.send('service_2ua9jwp','template_xc2i0cq',tempParams).then(function(res){
         console.log("sucess", res.status);
     });
+});
+
+blur.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="blur(5px)";
+});
+
+brightness.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="brightness(0.4)";
+});
+
+contrast.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="contrast(200%)";
+});
+
+invert.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="invert(50%)";
+});
+
+saturate.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="saturate(30%)";
+});
+
+sepia.addEventListener('click', () => {
+    document.querySelector('canvas').style.filter="sepia(60%)";
 });
